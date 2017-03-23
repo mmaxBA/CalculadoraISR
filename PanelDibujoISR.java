3bc67d6f2d7a37d0c3dd63497301738cc7a51f4b
Aby <danielaparralesd.m@gmail.com>
2017-03-23 17:57:55 -0600

    23_03_2017 panels
    El panel central de bienvenida ya está listo

--- PanelDibujoISR.java
+++ PanelDibujoISR.java
@@ -1,28 +1,62 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Label;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class PanelDibujoISR extends JPanel{
 	
-	private String saludo;
-
-		public PanelDibujoISR(){
+	//private String saludo=;
+	private JButton btIndividual,
+					btMultiples;
+	private Label saludo;
+	private Font txtBienvenida;
+	private Frame ventanaISR;
+		public PanelDibujoISR(VentanaISR frame){
 				super();
-				this.setPreferredSize(new Dimension(1000,800));
+				//t
+				this.setPreferredSize(new Dimension(1000,400));
 				this.setBackground(Color.GRAY);
-				this.saludo = "!Bienvenido a la calculadora del ISR¡";
+				//this.setLayout(null);
+			
+				
+				this.saludo= new Label("¡Bienvenido a la calculadora de ISR!",Label.CENTER);
+				this.setFont(new Font("Arial",Font.BOLD,36));
+				this.saludo.setForeground(new Color(0xffffdd));
+				this.saludo.setBackground(Color.GRAY);
+				this.saludo.setPreferredSize(new Dimension(800, 200));
+				this.add(saludo);
+				
+				
+				this.btIndividual=new JButton("Individual");
+				this.btIndividual.setFont(new Font("Calibri",Font.BOLD,26));
+				this.btIndividual.setPreferredSize(new Dimension(300, 150));
+				//this.btIndividual.setBounds(100,200,300,150);
+				this.btIndividual.setBackground(Color.WHITE);
+				this.add(btIndividual);
+				
+				
+				this.btMultiples=new JButton("Multiples");
+				this.btMultiples.setFont(new Font("Calibri",Font.BOLD,26));
+				this.btMultiples.setPreferredSize(new Dimension(300, 150));
+				//this.btMultiples.setBounds(600,200,300,150);
+				this.btMultiples.setBackground(Color.WHITE);
+				this.add(btMultiples);
+				
 				
 		}
-		public void componentes(Graphics g){
-			
-
+		/*public void paintComponent(Graphics g){
 			super.paintComponent(g);
-			
 			g.setColor(Color.WHITE);
-			g.drawString(saludo, 500, 600);
-		}
+			g.setFont(new Font("Arial",Font.BOLD,36));
+			g.drawString(saludo, (this.getWidth()/2)-280,(this.getHeight()/2)-80);
+		}*/
 }
--- VentanaISR.java
+++ VentanaISR.java
@@ -1,4 +1,6 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import java.awt.Graphics;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
@@ -7,13 +9,14 @@ public class VentanaISR extends JFrame{
 	
 	public VentanaISR() {
 		super("Calculador de ISR");
+		//this.setLocationRelativeTo();
+		//this.setPreferredSize(new Dimension(1000,800));
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		PanelDibujoISR pd = new PanelDibujoISR();
-		this.add(pd);
+		PanelDibujoISR pd = new PanelDibujoISR(this);
+		this.add(pd,BorderLayout.CENTER);
 		this.pack();
 		this.setVisible(true);
-		
-		
+				
 	}
 	public static void main(String []args){
 		VentanaISR window = new VentanaISR();
